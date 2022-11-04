baf3c4e733b173219c307d3301d11b23c053e15e
HuynhTri0811 <huynhhuutri0811>
2022-11-04 13:17:04 +0700

    HuynhTri

---
+++ ERP.Module.Win/Controllers/0.ExecuteImport/ImportClass/2.TienLuong/Imp_PhuCapTruongBoMon.cs
@@ -0,0 +1,185 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Xpo;
+using ERP.Module.Commons;
+using ERP.Module.Enum.Systems;
+using ERP.Module.Extends;
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using ERP.Module.NghiepVu.TienLuong.Luong;
+using ERP.Module.NghiepVu.NhanSu.NhanViens;
+using ERP.Module.DanhMuc.TienLuong;
+using ERP.Module.NghiepVu.TienLuong.ThuNhapKhac;
+using ERP.Module.NghiepVu.NhanSu.BoPhans;
+using ERP.Module.NghiepVu.NhanSu.HoSoLuong;
+using ERP.Module.NghiepVu.TienLuong.Thuong;
+//
+namespace ERP.Module.Controllers.Win.ExecuteImport.ImportClass.TienLuong
+{
+    public class Imp_PhuCapTruongBoMon
+    {        
+        public static void ImportPhuCapTruongBoMon(IObjectSpace obs, PhuCapTruongBoMon obj, LoaiOfficeEnum loaiOffice)
+        {
+            //
+            int sucessNumber = 0;
+            int erorrNumber = 0;
+            bool sucessImport = true;
+            StringBuilder mainLog = new StringBuilder();
+            DateTime ngayLap = DateTime.Now;
+            if (obj.KyTinhLuong != null)
+                ngayLap = obj.KyTinhLuong.DenNgay.AddDays(1);
+            //
+
+            using (OpenFileDialog open = new OpenFileDialog())
+            {
+                open.Filter = "Excel 2003 file (*.xls)|*.xls;*.xlsx";
+                //
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    using (DialogUtil.AutoWait())
+                    {
+                        using (DataTable dt = DataProvider.GetDataTableFromExcel(open.FileName, "[Sheet1$A1:G]", loaiOffice))
+                        {
+                            /////////////////////////////KHỞI TẠO CÁC IDX/////////////////////////////////////////////////////
+
+                            #region Khởi tạo các idx
+                            int idx_STT = 0;
+                            int idx_MaQuanLy = 1;
+                            int idx_HoTen = 2;
+                            int idx_DonVi = 3;
+                            int idx_NgayLap = 4;
+                            int idx_SoTien = 5;
+                            int idx_GhiChu = 6 ;                                        
+                            #endregion
+
+                            /////////////////////////////TIẾN HÀNH ĐỌC DỮ LIỆU TỪ EXCEL///////////////////////////////////////
+                            using (UnitOfWork uow = new UnitOfWork(((XPObjectSpace)obs).Session.DataLayer))
+                            {
+                                //
+                                uow.BeginTransaction();                              
+
+
+
+                                //Duyệt qua tất cả các dòng trong file excel
+                                foreach (DataRow dr in dt.Rows)
+                                {
+                                    StringBuilder detailLog = new StringBuilder();
+
+                                    //////////////////////////ĐỌC DỮ LIỆU//////////////////////////////////////
+                                    #region Đọc dữ liệu
+                                    string txt_STT = dr[idx_STT].ToString().Trim();
+                                    string txt_MaQuanLy = dr[idx_MaQuanLy].ToString().Trim();
+                                    string txt_HoTen = dr[idx_HoTen].ToString().Trim();
+                                    string txt_DonVi = dr[idx_DonVi].ToString().Trim();
+                                    string txt_NgayLap = dr[idx_NgayLap].ToString().Trim();
+                                    string txt_SoTien = dr[idx_SoTien].ToString().Trim();
+                                    string txt_GhiChu = dr[idx_GhiChu].ToString().Trim();
+                                    #endregion
+
+                                    //////////////////////////KIỂM TRA VÀ LẤY DỮ LIỆU//////////////////////////
+                                    #region Kiểm tra dữ
+                                    //
+                                    #region 1. Mã quản lý
+                                    if (!string.IsNullOrEmpty(txt_MaQuanLy))
+                                    {
+                                        NhanVien nhanVien_qd = uow.FindObject<NhanVien>(CriteriaOperator.Parse("CongTy = ? and (MaNhanVien like ? or MaTapDoan like ?)", obj.CongTy.Oid, txt_MaQuanLy, txt_MaQuanLy));
+                                        if (nhanVien_qd == null)
+                                        {
+                                            mainLog.AppendLine("- STT: " + txt_STT);
+                                            mainLog.AppendLine(string.Format("- Mã quản lý :{0} của nhân viên : {1} không tồn tại trong hệ thống.", txt_MaQuanLy, txt_HoTen));
+                                            //
+                                            sucessImport = false;
+                                        }
+                                        else
+                                        {
+                                            //
+                                            if (nhanVien_qd != null)
+                                            {
+                                                ChiTietPhuCapTruongBoMon _ChiTiet = new ChiTietPhuCapTruongBoMon(uow);
+                                                _ChiTiet.NhanVien = uow.GetObjectByKey<NhanVien>(nhanVien_qd.Oid);
+                                                _ChiTiet.SoTien = decimal.Parse(txt_SoTien);
+                                                _ChiTiet.PhuCapTruongBoMon = uow.GetObjectByKey<PhuCapTruongBoMon>(obj.Oid);
+                                            }
+                                            else
+                                            {
+                                                detailLog.AppendLine(string.Format("- Nhân viên Mã: {0} Tên: {1} không thuộc đơn vị quản lý ", nhanVien_qd.MaNhanVien, nhanVien_qd.HoTen));
+                                            }
+
+                                            #region Ghi File log
+                                            {
+                                                //Đưa thông tin bị lỗi vào blog
+                                                if (detailLog.Length > 0)
+                                                {
+                                                    mainLog.AppendLine("- STT: " + txt_STT);
+                                                    mainLog.AppendLine(string.Format("- Nhân viên Mã: {0} Tên: {1} không import vào phần mềm được: ", nhanVien_qd.MaNhanVien, nhanVien_qd.HoTen));
+                                                    mainLog.AppendLine(detailLog.ToString());
+                                                    //
+                                                    sucessImport = false;
+                                                }
+                                            }
+                                            #endregion
+                                        }
+                                    }
+                                    else
+                                    {
+                                        mainLog.AppendLine("- STT: " + txt_STT);
+                                        mainLog.AppendLine(string.Format("- Mã quản lý của nhân viên : {0} không được trống.", txt_HoTen));
+                                        //
+                                        sucessImport = false;
+                                    }
+                                    //
+                                    #endregion
+
+                                    #endregion
+                                    ///////////////////////////NẾU THÀNH CÔNG THÌ SAVE/////////////////////////////////
+                                    if (sucessImport)
+                                    {
+                                        //Lưu
+                                        //
+                                        sucessNumber++;
+                                    }
+                                    else
+                                    {
+                                        erorrNumber++;
+                                        //
+                                        sucessImport = true;
+                                    }
+                                }
+                                // End Duyệt qua tất cả các dòng trong file excel
+                                
+                                //
+                                string s = (erorrNumber > 0 ? "Mời bạn xem file log" : "");
+                                DialogUtil.ShowInfo("Import Thành Công " + sucessNumber + " dòng dữ liệu - Số dòng không thành công " + erorrNumber + " " + s + "!");
+
+                                if(erorrNumber == 0)
+                                {
+                                    uow.CommitChanges();
+
+                                }
+                                //Mở file log lỗi lên
+                                if (erorrNumber > 0)
+                                {
+                                    uow.RollbackTransaction();
+                                    string tenFile = "Import_Log.txt";
+                                    StreamWriter writer = new StreamWriter(tenFile);
+                                    writer.WriteLine(mainLog.ToString());
+                                    writer.Flush();
+                                    writer.Close();
+                                    writer.Dispose();
+                                    Common.WriteDataToFile(tenFile, mainLog.ToString());
+                                    Process.Start(tenFile);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
